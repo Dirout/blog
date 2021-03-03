f31e7a81c8eb368ddb0a960569c6cd0487534f6c
Emil Sayahi <32737166+limeschool@users.noreply.github.com>
2021-03-02 20:54:34 -0500

    Require permissions to install Sass CLI

--- postbuild.sh
+++ postbuild.sh
@@ -1,4 +1,4 @@
-npm install -g sass
+sudo npm install -g sass
 /bin/cp -ar ./mokk/assets ./gh-pages
 /bin/cp -ar ./mokk/robots.txt ./gh-pages
 sass ./mokk/_sass/main.scss ./gh-pages/assets/css/style.css
