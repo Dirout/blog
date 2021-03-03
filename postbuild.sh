2e5c2137502c127e4102f6bb36b55e71376b723d
Emil Sayahi <limesayahi@gmail.com>
2021-03-02 20:50:28 -0500

    Switching from 'basic' to 'Klisé' design

--- postbuild.sh
+++ postbuild.sh
@@ -1,2 +1,4 @@
-cp -ar ./mokk/assets ./gh-pages
-cp -ar ./mokk/robots.txt ./gh-pages
+npm install -g sass
+/bin/cp -ar ./mokk/assets ./gh-pages
+/bin/cp -ar ./mokk/robots.txt ./gh-pages
+sass ./mokk/_sass/main.scss ./gh-pages/assets/css/style.css
