0e17eae832984df9be13db3123e1000806b30c04
Emil Sayahi <32737166+limeschool@users.noreply.github.com>
2021-04-14 14:48:30 -0400

    Initial Netlify CMS commit

---
+++ admin/netlify.sh
@@ -0,0 +1,12 @@
+curl -s https://raw.githubusercontent.com/Dirout/dokkoo/master/Cargo.toml --output Cargo.toml > /dev/null
+version=$( awk -F ' = ' '$1 ~ /version/ { gsub(/[\"]/, "", $2); printf("%s",$2) }' Cargo.toml )
+curl -s -L https://github.com/Dirout/dokkoo/releases/download/v${version}/dokkoo_${version}_amd64.deb --output dokkoo_${version}_amd64.deb > /dev/null
+sudo apt-get -qq install ./dokkoo_${version}_amd64.deb > /dev/null
+printf "Installed Dokkoo v${version}"
+dokkoo build .
+sudo npm install -g sass
+/bin/cp -ar ./assets ./output
+/bin/cp -ar ./robots.txt ./output
+sass ./_sass/main.scss ./output/assets/css/style.css
+/bin/cp -ar ./admin/config.yml ./output/admin/config.yml
+touch .nojekyll
